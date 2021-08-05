28d2818f5aa052db188b5d56c777ea51850a2852
Bella and Mehayla <mehaylac@gmail.com>
2021-08-05 00:34:00 +0000

    Squeky clean code

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -5,22 +5,13 @@
 
 function showFortune(evt) {
 
-    // TODO: get the fortune and show it in the #fortune-text div
-    // $.get('/adjective', (response) => {
-    //   $('#adjective').text(response);
-    // });
-
-    $.get('/fortune', (evt) => {
-        $('#fortune-text').html(evt);
+    $.get('/fortune', (res) => {
+        $('#fortune-text').html(res);
     });
 }
 
 $('#get-fortune-button').on('click', showFortune);
     
-
-
-
-
 
 // PART 2: SHOW WEATHER
 
@@ -31,35 +22,34 @@ function showWeather(evt) {
     let formData = {"zipcode": $("#zipcode-field").val()};
 
 
-    $.get(url,formData,(evt) => {
+    $.get(url,formData,(res) => {
 
-        $('#weather-info').html(`${evt.forecast}`);
+        $('#weather-info').html(`${res.forecast}`);
 
     });
 }
 
 $("#weather-form").on('submit', showWeather);
 
-// alert(`Forecast for today is ${evt.forecast}.`)
-        // $('#weather-info').text({evt.forecast});
-
-// $.get('/delivery-info.json', formData, (res) => {
-//     // Display response from the server
-//     alert(`This will cost $${res.cost}`);
-//          $('#weather-info').text(${(evt.forecast))}
-//     alert(`This will arrive in ${res.days} day(s)`);
-//   });
 
 
 // PART 3: ORDER MELONS
 
 function orderMelons(evt) {
     evt.preventDefault();
+    
+    const formInputs = {
+        'melon_type':$('#melon-type-field').val(),
+        'qty':$('#qty-field').val()
+    };
 
-    // TODO: show the result message after your form
-    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+    $.post('/order-melons.json', formInputs, (res) => {
+        if (res.code === 'ERROR') {
+            $('#order-status').addClass('order-error');
+        }
+        $('#order-status').html(`${res.code} and ${res.msg}`); 
+
+    });
 }
 
 $("#order-form").on('submit', orderMelons);
-
-
